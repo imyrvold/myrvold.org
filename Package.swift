f76d5bb8bde4c7e8674355a3fb89928f4f96fe08
Ivan C Myrvold <ivan.myrvold@nov.com>
2020-04-12 17:33:18 +0200

    initial commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "IrinaBlog",
+    products: [
+        .executable(name: "IrinaBlog", targets: ["IrinaBlog"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.3.0"),
+        .package(url: "https://github.com/dinsen/brianpublishtheme", from: "0.1.0")
+    ],
+    targets: [
+        .target(
+            name: "IrinaBlog",
+            dependencies: ["Publish", "BrianPublishTheme"]
+        )
+    ]
+)
---
+++ Sources/IrinaBlog/main.swift
@@ -0,0 +1,38 @@
+import Foundation
+import Publish
+import Plot
+//import BrianPublishTheme
+
+// This type acts as the configuration for your website.
+struct IrinaBlog: Website {
+    enum SectionID: String, WebsiteSectionID {
+        // Add the sections that you want your website to contain here:
+        case posts
+    }
+
+    struct ItemMetadata: WebsiteItemMetadata {
+        // Add any site-specific metadata that you want to use here.
+    }
+
+    // Update these properties to configure your website:
+    var url = URL(string: "https://www.myrvold.org")!
+    var name = "Блог Ирины"
+    var description = "Моя жизнь в Норвегии как русский"
+    var language: Language { .russian }
+    var imagePath: Path? { nil }
+}
+
+// This will generate your website using the built-in Foundation theme:
+//try IrinaBlog().publish(withTheme: .brian)
+try IrinaBlog().publish(withTheme: .foundation,
+additionalSteps: [
+    .deploy(using: .gitHub("imyrvold/imyrvold.github.io"))
+])
+//try IrinaBlog().publish(using: [
+//       .addMarkdownFiles(),
+//       .copyResources(),
+//       .generateHTML(withTheme: .foundation),
+//       .generateRSSFeed(including: [.posts]),
+//       .generateSiteMap(),
+//       .deploy(using: .gitHub("imyrvold/imyrvold.github.io.git"))
+//])
